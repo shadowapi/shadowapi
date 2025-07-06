c002741dae1b0b9539f242f7493e9dd5be035324
reactima <1@ilya1.com>
2025-07-07 01:41:56 +0300

    added lestrrat-go/jwx

--- backend/go.mod
+++ backend/go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/gotd/contrib v0.21.0
 	github.com/gotd/td v0.120.0
 	github.com/jackc/pgx/v5 v5.7.1
+	github.com/lestrrat-go/jwx/v2 v2.1.6
 	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/mdp/qrterminal v1.0.1
 	github.com/nats-io/nats.go v1.38.0
@@ -30,6 +31,7 @@ require (
 	go.opentelemetry.io/otel/trace v1.34.0
 	go.uber.org/multierr v1.11.0
 	go.uber.org/zap v1.27.0
+	golang.org/x/crypto v0.33.0
 	golang.org/x/oauth2 v0.24.0
 	google.golang.org/api v0.213.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -44,6 +46,7 @@ require (
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/coder/websocket v1.8.12 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.0 // indirect
 	github.com/dlclark/regexp2 v1.11.4 // indirect
 	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
 	github.com/fatih/color v1.18.0 // indirect
@@ -54,6 +57,7 @@ require (
 	github.com/go-faster/yaml v0.4.6 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.0 // indirect
@@ -68,6 +72,11 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.3 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.6 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/magiconair/properties v1.8.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -96,12 +105,11 @@ require (
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/exp v0.0.0-20250215185904-eff6e970281f // indirect
 	golang.org/x/mod v0.23.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/tools v0.30.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241216192217-9240e9c98484 // indirect
--- backend/internal/zitadel/api_token.go
+++ backend/internal/zitadel/api_token.go
@@ -1,85 +1,107 @@
 package zitadel
 
-//
-//import (
-//	"context"
-//	"crypto/rsa"
-//	"encoding/json"
-//	"encoding/pem"
-//	"errors"
-//	"io"
-//	"net/http"
-//	"net/url"
-//	"os"
-//	"strings"
-//	"time"
-//
-//	"github.com/google/uuid"
-//	"github.com/lestrrat-go/jwx/v2/jwa"
-//	"github.com/lestrrat-go/jwx/v2/jwk"
-//	"github.com/lestrrat-go/jwx/v2/jwt"
-//	"golang.org/x/oauth2"
-//)
-//
-//type apiKeyJSON struct {
-//	KeyID    string `json:"keyId"`
-//	Key      string `json:"key"`
-//	ClientID string `json:"clientId"`
-//}
-//
-//func loadAPIKey(p string) (*apiKeyJSON, *rsa.PrivateKey, error) {
-//	f, err := os.Open(p)
-//	if err != nil {
-//		return nil, nil, err
-//	}
-//	defer f.Close()
-//	raw, _ := io.ReadAll(f)
-//	var k apiKeyJSON
-//	if err := json.Unmarshal(raw, &k); err != nil {
-//		return nil, nil, err
-//	}
-//	b, _ := pem.Decode([]byte(k.Key))
-//	if b == nil {
-//		return nil, nil, errors.New("pem decode")
-//	}
-//	key, err := jwk.ParseKey(b.Bytes, jwk.WithPEM(true))
-//	if err != nil {
-//		return nil, nil, err
-//	}
-//	var rsaKey *rsa.PrivateKey
-//	if err := key.Raw(&rsaKey); err != nil {
-//		return nil, nil, err
-//	}
-//	return &k, rsaKey, nil
-//}
-//
-//func (c *Client) TokenWithJWTProfile(ctx context.Context, scope string) (*oauth2.Token, error) {
-//	meta, priv, err := loadAPIKey(c.cfg.Auth.Zitadel.APIKeyFile)
-//	if err != nil {
-//		return nil, err
-//	}
-//	j, _ := jwt.NewBuilder().
-//		Issuer(meta.ClientID).
-//		Subject(meta.ClientID).
-//		Audience([]string{c.cfg.Auth.Zitadel.InstanceURL + "/oauth/v2/token"}).
-//		IssuedAt(time.Now()).
-//		Expiration(time.Now().Add(5*time.Minute)).
-//		Claim("jti", uuid.NewString()).
-//		Build()
-//	signed, _ := jwt.Sign(j, jwt.WithKey(jwa.RS256, priv))
-//	v := url.Values{}
-//	v.Set("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer")
-//	v.Set("assertion", string(signed))
-//	if scope != "" {
-//		v.Set("scope", scope)
-//	}
-//	req, _ := http.NewRequestWithContext(ctx, http.MethodPost, c.cfg.Auth.Zitadel.InstanceURL+"/oauth/v2/token", strings.NewReader(v.Encode()))
-//	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-//	res, err := c.client.Do(req)
-//	if err != nil {
-//		return nil, err
-//	}
-//	defer res.Body.Close()
-//	var tok oauth2.Token
-//	return &tok, json.NewDecoder(res.Body).Decode(&tok)
-//}
+import (
+	"context"
+	"crypto/rsa"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+	"golang.org/x/oauth2"
+)
+
+type apiKeyJSON struct {
+	KeyID    string `json:"keyId"`
+	Key      string `json:"key"`
+	ClientID string `json:"clientId"`
+}
+
+func loadAPIKey(p string) (*apiKeyJSON, *rsa.PrivateKey, error) {
+	f, err := os.Open(p)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer f.Close()
+	raw, err := io.ReadAll(f)
+	if err != nil {
+		return nil, nil, err
+	}
+	var k apiKeyJSON
+	if err = json.Unmarshal(raw, &k); err != nil {
+		return nil, nil, err
+	}
+	block, _ := pem.Decode([]byte(k.Key))
+	if block == nil {
+		return nil, nil, errors.New("pem decode failed")
+	}
+	jwkKey, err := jwk.ParseKey(block.Bytes, jwk.WithPEM(true))
+	if err != nil {
+		return nil, nil, err
+	}
+	var priv *rsa.PrivateKey
+	if err = jwkKey.Raw(&priv); err != nil {
+		return nil, nil, err
+	}
+	return &k, priv, nil
+}
+
+// TokenWithJWTProfile issues an access token for backend-to-backend calls.
+func (c *Client) TokenWithJWTProfile(ctx context.Context, scope string) (*oauth2.Token, error) {
+	if c.cfg.Auth.Zitadel.APIKeyFile == "" {
+		return nil, errors.New("zitadel.api_key_file not configured")
+	}
+	meta, priv, err := loadAPIKey(c.cfg.Auth.Zitadel.APIKeyFile)
+	if err != nil {
+		return nil, err
+	}
+	audience := c.cfg.Auth.Zitadel.InstanceURL + "/oauth/v2/token"
+	claims, err := jwt.NewBuilder().
+		Issuer(meta.ClientID).
+		Subject(meta.ClientID).
+		Audience([]string{audience}).
+		IssuedAt(time.Now()).
+		Expiration(time.Now().Add(5*time.Minute)).
+		Claim("jti", uuid.NewString()).
+		Build()
+	if err != nil {
+		return nil, err
+	}
+	signed, err := jwt.Sign(claims, jwt.WithKey(jwa.RS256, priv))
+	if err != nil {
+		return nil, err
+	}
+	form := url.Values{
+		"grant_type": {"urn:ietf:params:oauth:grant-type:jwt-bearer"},
+		"assertion":  {string(signed)},
+	}
+	if scope != "" {
+		form.Set("scope", scope)
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, audience, strings.NewReader(form.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(res.Body)
+		return nil, fmt.Errorf("token status %d: %s", res.StatusCode, body)
+	}
+	var tok oauth2.Token
+	return &tok, json.NewDecoder(res.Body).Decode(&tok)
+}
